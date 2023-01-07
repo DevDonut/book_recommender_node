9796f7ff3bab1041d2afa4413803082a29e4bde1
DevDonut <41371020+DevDonut@users.noreply.github.com>
2023-01-07 12:03:57 +0100

    Add simple username login

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const session = require('express-session')
 
 app.engine('html', require('ejs').renderFile)
 app.use(express.static('public'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
-    res.render('index.html')
+    if (req.session.loggedin) {
+        res.redirect('/home')
+    }
+    else {
+        res.redirect('/login')
+    }
 })
 
 app.get('/login', function(req, res) {
     res.render('login.html')
+})
+
+app.post('/auth', function(request, response) {
+    let username = request.body.username
+    if (username) {
+        request.session.loggedin = true
+        request.session.username = username
+        response.redirect('/home')
+    }
+})
+
+app.get('/home', function(req, res) {
+    if (req.session.loggedin) {
+        res.send(req.session.username)
+    }
+    else {
+        res.redirect('/login')
+    }
 })
 
 app.listen(3000)
